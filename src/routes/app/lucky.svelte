b0fb990ee570a197cf4747910e9482e4068526a3
Luyu Cheng <chengluyu@live.cn>
2021-05-07 22:03:19 +0800

    feat: implement lucky page and wander page

---
+++ src/routes/app/lucky.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  function range(n: number): number[] {
+    const numbers = [];
+    for (let i = 0; i < n; i++) {
+      numbers.push(n);
+    }
+    return numbers;
+  }
+</script>
+
+<p>这些标签未曾记录，却隐藏在文本之后</p>
+
+<section class="grid grid-cols-1 md:grid-cols-2 grid-rows-8 gap-x-6 gap-y-4">
+  {#each range(16) as _}
+    <article class="p-4 flex items-center bg-gray-50 rounded-md">
+      <div>■■■■■</div>
+      <div class="ml-auto text-gray-500">42 memos</div>
+    </article>
+  {/each}
+</section>
---
+++ src/routes/app/notify.svelte
@@ -0,0 +1,11 @@
+<script lang="ts"></script>
+
+<section class="px-4 py-6 bg-gray-50 space-y-2">
+  <strong class="text-gray-900 font-semibold">设置回顾规则</strong>
+  <p class="text-gray-400">
+    最多 3 条回顾规则，助你每日更好回顾过往。<a class="text-blue-500" href="/">更多用法…</a>
+  </p>
+  <button class="px-6 py-1 bg-yellow-500 text-gray-50 font-semibold rounded">
+    续费会员继续使用
+  </button>
+</section>
