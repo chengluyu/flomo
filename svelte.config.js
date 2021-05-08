9d518d416dca2b965983143fc1ba47f1e4683871
Luyu Cheng <chengluyu@live.cn>
2021-05-08 14:00:42 +0800

    feat(deploy): support vercel build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,6 +16,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    adapter: process.env.ADAPTER === 'vercel' ? vercel() : undefined
   }
 };
 
