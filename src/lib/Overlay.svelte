539cf8b9704e2bbde330f74ac79a23e012596033
Luyu Cheng <chengluyu@live.cn>
2021-05-08 12:22:01 +0800

    feat: add the preference menu

---
+++ src/lib/Overlay.svelte
@@ -0,0 +1,10 @@
+<div
+  class="block fixed top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-5 z-40"
+  tabindex="0"
+  role="button"
+  aria-pressed="false"
+  on:click
+  on:wheel={(e) => {
+    e.preventDefault();
+  }}
+/>
--- src/lib/aside/Header.svelte
+++ src/lib/aside/Header.svelte
@@ -1,12 +1,60 @@
 <script>
   import Preferences from '$lib/icons/Preferences.svelte';
+  import Overlay from '$lib/Overlay.svelte';
   import ProBadge from './_ProBadge.svelte';
+
+  let showMenu = false;
 </script>
 
 <section class="flex flex-row items-center">
   <div class="text-lg text-gray-600 font-medium">User</div>
   <ProBadge />
-  <button class="ml-auto text-gray-600">
-    <Preferences />
-  </button>
+  <div class="ml-auto relative">
+    <button
+      class="text-gray-600 focus:outline-none"
+      on:click={() => {
+        showMenu = !showMenu;
+      }}
+    >
+      <Preferences />
+    </button>
+    {#if showMenu}
+      <Overlay
+        on:click={() => {
+          showMenu = false;
+        }}
+      />
+      <div class="menu space-y-2" role="menu">
+        <button><span role="img" aria-label="user">👤</span>账号详情</button>
+        <button><span role="img" aria-label="gift">🎁</span>有些福利</button>
+        <button><span role="img" aria-label="tools">🛠</span>API 及第三方工具</button>
+        <button><span role="img" aria-label="weibo">👁️</span>官方微博</button>
+        <button><span role="img" aria-label="help">🤔</span>帮助</button>
+        <button><span role="img" aria-label="feedback">👾</span>反馈</button>
+        <button><span role="img" aria-label="bye">👋</span>退出</button>
+      </div>
+    {/if}
+  </div>
 </section>
+
+<style style lang="postcss">
+  .menu {
+    @apply absolute top-10 left-1/2 w-max;
+    @apply px-5 py-4;
+    @apply bg-white;
+    @apply flex flex-col items-start;
+    @apply rounded-md shadow-md;
+    @apply z-50;
+    @apply transform -translate-x-1/2;
+  }
+
+  .menu span[role='img'] {
+    @apply mr-2;
+  }
+
+  .menu > button {
+    @apply w-full;
+    @apply text-left;
+    @apply focus:outline-none;
+  }
+</style>
